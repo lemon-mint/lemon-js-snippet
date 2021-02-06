a73f209c06d88eb65da44c1d223ba53fa23aa8ca
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-02-06 11:01:15 +0900

    Update open_server_list.js

--- servertime/open_server_list.js
+++ servertime/open_server_list.js
@@ -1,5 +1,9 @@
 const server_time_server = [
     "https://pbyvddianolj.runkit.sh/",
     "https://d6ln9wldoewo.runkit.sh/",
-    "https://vwjdh7t6fw1y.runkit.sh/"
+    "https://vwjdh7t6fw1y.runkit.sh/",
+    "https://vsbuj20jvzuq.runkit.sh/",
+    "https://0jdb34nm4yz0.runkit.sh/",
+    "https://wb8yu1gbzymy.runkit.sh/",
+    "https://urzx7twa8vv8.runkit.sh/"
 ]
